5aa4e5f39434e091ea971e41532f4ea4b702cafc
dbie17 <biedionisio@gmail.com>
2017-09-05 16:04:36 +0200

    14h

--- src/exer1/Binario.java
+++ src/exer1/Binario.java
@@ -10,27 +10,18 @@ package exer1;
  * @author home
  */
 public class Binario {
-    public static void main(String[] args) 
-	{
-				int x=13;
-				int r=0;
-				String b="";
-				System.out.println("x:"+x);
-				b=Integer.toBinaryString(x);
-				System.out.println("b:"+b);
-				System.out.println("Verificao:"+Integer.parseInt(b,2));
-				System.out.println("......");
-				//Operador shift
-				r=x<<1;
-				System.out.println("r:"+r);
-				b=Integer.toBinaryString(r);
-				System.out.println("b:"+b);
-				System.out.println("Verificao: "+Integer.parseInt(b,2));
-				System.out.println("......");
-				r=x<<1;
-				System.out.println("r: "+r);
-				b=Integer.toBinaryString(r);
-				System.out.println("b: "+b);
-				System.out.println("Verificao: "+Integer.parseInt(b,2));
-			}
+    
+    
+    static void binario(int n){
+        if (n>0){
+            binario (n/2);
+            
+            System.out.println(n%2);
+        }
+    }
+    
+    public static void main(String[] args) {
+        binario(20);
+			
+}
 }
